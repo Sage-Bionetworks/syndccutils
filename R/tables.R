0fef821b7056e684928d6c252946043899ceb436
Sara JC Gosline <sara.gosline@sagebase.org>
2017-09-12 09:38:59 -0700

    updated with queries inline

--- R/tables.R
+++ R/tables.R
@@ -4,7 +4,7 @@ library(synapseClient)
 # Project summary tables --------------------------------------------------
 
 # adapted from lines 29-33 in 'fileViewReporting.Rmd'
-summarize_project_info <- function(view_df) {
+summarize_project_info <- function(view_df,fv_synid) {
     proj_info <-
         sapply(unique(c(view_df$projectId)),
                function(x) {
@@ -22,7 +22,7 @@ summarize_project_info <- function(view_df) {
 }
 
 # adapted from lines 40-44 in 'fileViewReporting.Rmd'
-summarize_project_datafile_counts <- function(view_df, project_info) {
+summarize_project_datafile_counts <- function(view_df, project_info,fv_synid) {
     project_datafile_counts <- view_df %>%
         group_by(projectId) %>%
         summarize(
@@ -34,11 +34,16 @@ summarize_project_datafile_counts <- function(view_df, project_info) {
             Patients = n_distinct(individualID)
         ) %>%
         inner_join(project_info, ., by = "projectId")
-    project_datafile_counts
-}
+    ###adding query information
+    fv_syn_query=paste('SELECT * FROM',fv_synid,'WHERE')
+    dl_syn_query=paste('SELECT id FROM',fv_synid,'WHERE')
+
+    project_datafile_counts%>% mutate(viewFiles=paste(fv_syn_query,'projectId =',paste0('"',projectId,'"')),
+        downloadFiles=paste(dl_syn_query,'projectId =',paste0('"',projectId,'"')))
+    }
 
 # adapted from lines 49-53 in 'fileViewReporting.Rmd'
-summarize_project_toolfile_counts <- function(view_df, project_info) {
+summarize_project_toolfile_counts <- function(view_df, project_info,fv_synid) {
     project_toolfile_counts <- view_df %>%
         group_by(projectId) %>%
         summarize(
@@ -48,24 +53,39 @@ summarize_project_toolfile_counts <- function(view_df, project_info) {
         ) %>%
         inner_join(project_info, ., by = "projectId") %>%
         mutate(Label = paste(Institution, Program, sep = '\n'))
-    project_toolfile_counts
+
+    ###adding query information
+    fv_syn_query=paste('SELECT * FROM',fv_synid,'WHERE')
+    dl_syn_query=paste('SELECT id FROM',fv_synid,'WHERE')
+
+    project_toolfile_counts%>% mutate(viewFiles=paste(fv_syn_query,'projectId =',paste0('"',projectId,'"')),
+        downloadFiles=paste(dl_syn_query,'projectId =',paste0('"',projectId,'"')))
+
 }
 
 # adapted from lines 37-43 in 'toolTypeReporting.Rmd'
-summarize_project_toollanguage_counts <- function(view_df, project_info) {
+summarize_project_toollanguage_counts <- function(view_df, project_info,fv_synid) {
     project_toollanguage_counts = view_df %>%
         group_by(softwareLanguage, projectId) %>%
         summarize(Files = n_distinct(id)) %>%
         inner_join(project_info, ., by = "projectId") %>%
         mutate(Label = paste(Institution, Program, sep = '\n'))
 
-    arrange(project_toollanguage_counts, desc(Files))
+    project_toollanguage_counts <- arrange(project_toollanguage_counts, desc(Files))
+
+    ###adding query information
+    fv_syn_query=paste('SELECT * FROM',fv_synid,'WHERE')
+    dl_syn_query=paste('SELECT id FROM',fv_synid,'WHERE')
+
+    project_toollanguage_counts%>% mutate(viewFiles=paste(fv_syn_query,'projectId =',paste0('"',projectId,'"'),'AND softwareLanguage =',paste0('"',softwareLanguage,'"')),
+        downloadFiles=paste(dl_syn_query,'projectId =',paste0('"',projectId,'"'),'AND softwareLanguage =',softwareLanguage))
+
 }
 
 # Data type tables --------------------------------------------------------
 
 # adapted from lines 36-38 in 'dataTypeReporting.Rmd'
-summarize_assay_counts <- function(fileview_df) {
+summarize_assay_counts <- function(fileview_df,fv_synid) {
     assay_counts = fileview_df %>%
         group_by(assay) %>%
         summarize(
@@ -76,11 +96,19 @@ summarize_assay_counts <- function(fileview_df) {
             specimens = n_distinct(specimenID),
             patients = n_distinct(individualID)
         )
-    arrange(assay_counts, desc(Centers))
+    assay_counts <- arrange(assay_counts, desc(Centers))
+
+    ###adding query information
+    fv_syn_query=paste('SELECT * FROM',fv_synid,'WHERE')
+    dl_syn_query=paste('SELECT id FROM',fv_synid,'WHERE')
+
+    assay_counts%>% mutate(viewFiles=paste(fv_syn_query,'assay =',paste0('"',assay,'"')),
+        downloadFiles=paste(dl_syn_query,'assay =',paste0('"',assay,'"')))
 }
 
 # adapted from lines 42-44 in 'dataTypeReporting.Rmd'
-summarize_assay_stats <- function(fileview_df) {
+summarize_assay_stats <- function(fileview_df,fv_synid) {
+
     assay_stats = fileview_df %>%
         group_by(assay, tumorType, diagnosis) %>%
         summarize(
@@ -90,11 +118,18 @@ summarize_assay_stats <- function(fileview_df) {
             patients = n_distinct(individualID)
         )
 
-    arrange(assay_stats, desc(Centers))
+    assay_stats=arrange(assay_stats, desc(Centers))
+
+    ###adding query information
+    fv_syn_query=paste('SELECT * FROM',fv_synid,'WHERE')
+    dl_syn_query=paste('SELECT id FROM',fv_synid,'WHERE')
+
+    assay_stats%>% mutate(viewFiles=paste(fv_syn_query,'tumorType =',paste0('"',tumorType,'"'),'AND assay =',paste0('"',assay,'"'),'AND diagnosis =',paste0('"',diagnosis,'"')),
+        downloadFiles=paste(dl_syn_query,'tumorType =',paste0('"',tumorType,'"'),'AND assay =',paste0('"',assay,'"'),'AND diagnosis =',paste0('"',diagnosis,'"')))
 }
 
 # adapted from lines 70-72 in 'dataTypeReporting.Rmd'
-summarize_disease_counts <- function(fileview_df) {
+summarize_disease_counts <- function(fileview_df,fv_synid) {
     disease_counts = fileview_df %>%
         group_by(diagnosis) %>%
         summarize(
@@ -106,11 +141,17 @@ summarize_disease_counts <- function(fileview_df) {
             patients = n_distinct(individualID)
         )
 
-    arrange(disease_counts, desc(Centers))
+    disaese_counts <- arrange(disease_counts, desc(Centers))
+    ###adding query information
+    fv_syn_query=paste('SELECT * FROM',fv_synid,'WHERE')
+    dl_syn_query=paste('SELECT id FROM',fv_synid,'WHERE')
+
+    disease_counts%>% mutate(viewFiles=paste(fv_syn_query,'diagnosis =',paste0('"',diagnosis,'"')),
+        downloadFiles=paste(dl_syn_query,'diagnosis =',paste0('"',diagnosis,'"')))
 }
 
 # adapted from lines 77-79 in 'dataTypeReporting.Rmd'
-summarize_tumortype_counts <- function(fileview_df) {
+summarize_tumortype_counts <- function(fileview_df,fv_synid) {
     tt_counts = fileview_df %>%
         group_by(tumorType) %>%
         summarize(
@@ -122,13 +163,19 @@ summarize_tumortype_counts <- function(fileview_df) {
             patients = n_distinct(individualID)
         )
 
-    arrange(tt_counts, desc(Centers))
+    tt_counts <- arrange(tt_counts, desc(Centers))
+    ###adding query information
+    fv_syn_query=paste('SELECT * FROM',fv_synid,'WHERE')
+    dl_syn_query=paste('SELECT id FROM',fv_synid,'WHERE')
+
+    assay_stats%>% mutate(viewFiles=paste(fv_syn_query,'tumorType =',paste0('"',tumorType,'"')),
+        downloadFiles=paste(dl_syn_query,'tumorType =',paste0('"',tumorType,'"')))
 }
 
 # Patient summary tables --------------------------------------------------
 
-summarize_assaycounts_by_patient <- function(fileview_df) {
-    fileview_df %>%
+summarize_assaycounts_by_patient <- function(fileview_df,fv_synid) {
+    assay_patient <- fileview_df %>%
         group_by(individualID, assay) %>%
         tally() %>%
         ungroup() %>%
@@ -137,12 +184,17 @@ summarize_assaycounts_by_patient <- function(fileview_df) {
         replace_na(list(n = 0)) %>%
         # need to be careful with column types
         mutate(n = as.integer(n))
+    ###adding query information
+    fv_syn_query=paste('SELECT * FROM',fv_synid,'WHERE')
+    dl_syn_query=paste('SELECT id FROM',fv_synid,'WHERE')
+    assay_patient%>% mutate(viewFiles=paste(fv_syn_query,'individualID =',paste0('"',individualID,'"'),'AND assay =',paste0('"',assay,'"')),
+        downloadFiles=paste(dl_syn_query,'individualID =',paste0('"',individualID,'"'),'AND assay =',paste0('"',assay,'"')))
 
 }
 
 # adapted from lines 71-79 in 'pan_stanford_viz.Rmd'
-summarize_assay_datafile_counts_by_patient <- function(fileview_df) {
-    fileview_df %>%
+summarize_assay_datafile_counts_by_patient <- function(fileview_df,fv_synid) {
+    df_by_patient <- fileview_df %>%
         group_by(individualID, assay) %>%
         tally() %>%
         ungroup() %>%
@@ -152,11 +204,19 @@ summarize_assay_datafile_counts_by_patient <- function(fileview_df) {
         # need to be careful with column types
         mutate(n = as.integer(n)) %>%
         spread(individualID, n)
+
+    ###adding query information
+    fv_syn_query=paste('SELECT * FROM',fv_synid,'WHERE')
+    dl_syn_query=paste('SELECT id FROM',fv_synid,'WHERE')
+    df_by_patient%>% mutate(viewFiles=paste(fv_syn_query,'individualID =',paste0('"',individualID,'"'),'AND assay =',paste0('"',assay,'"')),
+        downloadFiles=paste(dl_syn_query,'individualID =',paste0('"',individualID,'"'),'AND assay =',paste0('"',assay,'"')))
+
 }
 
 # adapted from lines 81-89 in 'pan_stanford_viz.Rmd'
-summarize_patient_datafile_counts_by_assay <- function(fileview_df) {
-    fileview_df %>%
+summarize_patient_datafile_counts_by_assay <- function(fileview_df,fv_synid) {
+
+    stats_table <- fileview_df %>%
         group_by(individualID, assay) %>%
         tally() %>%
         ungroup() %>%
@@ -166,4 +226,11 @@ summarize_patient_datafile_counts_by_assay <- function(fileview_df) {
         # need to be careful with column types
         mutate(n = as.integer(n)) %>%
         spread(assay, n)
+
+    ###adding query information
+    fv_syn_query=paste('SELECT * FROM',fv_synid,'WHERE')
+    dl_syn_query=paste('SELECT id FROM',fv_synid,'WHERE')
+    stats_table%>% mutate(viewFiles=paste(fv_syn_query,'individualID =',paste0('"',individualID,'"'),'AND assay =',paste0('"',assay,'"')),
+            downloadFiles=paste(dl_syn_query,'individualID =',paste0('"',individualID,'"'),'AND assay =',paste0('"',assay,'"')))
+
 }
--- R/testCode.R
+++ R/testCode.R
@@ -7,17 +7,19 @@ source('./synapse_helpers.R')
 source("./tables.R")
 source('./charts.R')
 
-
-fv_df<-get_table_df('syn9630847')
+syn_id='syn9630847'
+fv_df<-get_table_df(syn_id)
 
 proj_info=summarize_project_info(fv_df)
-project_stats=summarize_project_datafile_counts(fv_df, proj_info)
+project_stats=summarize_project_datafile_counts(fv_df, proj_info,syn_id)
+wiki_markdown <- table_as_wiki(project_stats,columnsAsCode=c(12))
 
 p<-plot_project_file_counts_by_center(project_stats)
 
-#next up, write code to save files and embed html in wiki
 
-assay_stats <- summarize_assay_stats(fv_df)
+#next up, write code to save files and embed html in wiki
+assay_stats <- summarize_assay_stats(fv_df,syn_id)
+wiki_markdown <- table_as_wiki(assay_stats,columnsAsCode=c(9))
 p<- plot_assay_stats_by_disease(assay_stats)
 
 
