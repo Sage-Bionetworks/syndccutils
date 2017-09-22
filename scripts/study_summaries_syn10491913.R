6cb0c94df4e631fef9f03db746a944da5e0559f8
James Eddy <james.a.eddy@gmail.com>
2017-09-21 21:54:06 -0700

    update study and project scripts to use latest functions

--- scripts/study_summaries_syn10491913.R
+++ scripts/study_summaries_syn10491913.R
@@ -17,21 +17,31 @@ master_fileview_id <- "syn10531116" # Synapse fileview associated with study
 fileview_df <- get_table_df(master_fileview_id)
 
 
-# Files by assay -------------------------------------------------------
+# Data files by assay and tumor type --------------------------------------
 
 table_filename <- glue::glue("{source_id}_DataFileCountsByAssayAndTumorType.html",
                              source_id = study_id)
 
 # create and save table
-datafile_counts_by_assay_and_tumortype <- fileview_df %>%
-    summarize_datafiles_by_assay_and_tumortype(master_fileview_id)
+group_keys <- c("assay", "tumorType")
+count_cols <- c("id", "diagnosis", "individualID", "specimenID")
 
-datafile_counts_by_assay_and_tumortype_dt <- datafile_counts_by_assay_and_tumortype %>%
-    format_summarytable_columns(c("assay", "tumorType")) %>%
+datafile_counts <- fileview_df %>%
+    summarize_files_by_annotationkey(
+        annotation_keys = group_keys,
+        table_id = master_fileview_id,
+        count_cols = count_cols
+    )
+
+datafile_counts_dt <- datafile_counts %>%
+    format_summarytable_columns(group_keys) %>%
     as_datatable()
-datafile_counts_by_assay_and_tumortype_dt
-syn_dt_entity <- datafile_counts_by_assay_and_tumortype_dt %>%
+
+syn_dt_entity <- datafile_counts_dt %>%
     save_datatable(parent_id, table_filename, .)
+
+# view table
+datafile_counts_dt
 
 
 # Individuals by assays by tumor type -------------------------------------
@@ -41,20 +51,29 @@ chart_filename <- glue::glue("{source_id}_IndividualsByAssayAndTumorType.html",
 
 # create and save chart
 plot_keys <- list(assay = "Assay", tumorType = "Tumor Type")
+
 chart <- fileview_df %>%
     plot_sample_counts_by_annotationkey_2d(sample_key = "individualID",
                                            annotation_keys = plot_keys)
-# chart
+
 syn_chart_entity <- save_chart(parent_id, chart_filename, chart)
+
+# view chart
+chart
 
 
 # Files by category -------------------------------------------------------
 
 chart_filename <- glue::glue("{source_id}_DataFilesByCategory.html",
                              source_id = study_id)
+
+# create and save chart
 plot_keys <- list(assay = "Assay", tumorType = "Tumor Type")
 
 chart <- fileview_df %>%
     plot_file_counts_by_annotationkey(plot_keys)
-# chart
+
 syn_entity <- save_chart(parent_id, chart_filename, chart)
+
+# view chart
+chart
