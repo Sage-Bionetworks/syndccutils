818813459012fb586ab5ae4249686a690c8cb083
James Eddy <james.a.eddy@gmail.com>
2018-02-27 15:03:29 -0800

    add mock datatable for testing

--- R/test_data.R
+++ R/test_data.R
@@ -15,6 +15,7 @@ mock_fileview_df <- tibble(
 
 # paths to mocked files
 mock_chart_filename <- "mock_chart.html"
+mock_datatable_filename <- "mock_datatable.html"
 
 # create and save mock chart
 plot_keys <- list(data_annotation = "Assay", status_annotation = "Disease")
@@ -26,5 +27,29 @@ saveWidget(mock_chart, mock_chart_filename,
 file.rename(mock_chart_filename,
             file.path("tests/testthat/testdata", mock_chart_filename))
 dir_delete("mock_chart_files/")
+
+# create and save mock table
+group_keys <- c("data_annotation", "status_annotation")
+count_cols <- c("id", "id_annotation")
+list_cols <- "random_annotation"
+
+mock_datafile_counts <- mock_fileview_df %>%
+    summarize_files_by_annotationkey_new(
+        annotation_keys = group_keys,
+        table_id = "syn1234",
+        count_cols = count_cols,
+        list_cols = list_cols
+    )
+
+mock_datafile_counts_dt <- datafile_counts %>%
+    format_summarytable_columns(group_keys) %>%
+    as_datatable()
+
+
+saveWidget(mock_datafile_counts_dt, mock_datatable_filename,
+           selfcontained = FALSE)
+file.rename(mock_datatable_filename,
+            file.path("tests/testthat/testdata", mock_datatable_filename))
+dir_delete("mock_datatable_files/")
 
 
