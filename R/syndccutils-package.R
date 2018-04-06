e41a2d797dd150d666314c6ecd8ff9ab83f561ad
Kara Woo <woo.kara@gmail.com>
2018-04-05 17:15:00 -0700

    Import purrr functions that are used

--- R/syndccutils-package.R
+++ R/syndccutils-package.R
@@ -1,4 +1,6 @@
 #' @rawNamespace import(ggplot2, except = vars)
 #' @import dplyr
 #' @importFrom magrittr %>%
+#' @importFrom purrr keep has_element map map2 map_df map_chr map_int map_lgl
+#'   flatten flatten_chr set_names walk2
 NULL
