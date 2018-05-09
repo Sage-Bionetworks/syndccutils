e9433d03023a60522aa1e7f8016839abde6af4e1
Kara Woo <woo.kara@gmail.com>
2018-05-09 10:24:23 -0700

    Add some more tests

--- R/tests/testthat/test-manifest.R
+++ R/tests/testthat/test-manifest.R
@@ -23,12 +23,58 @@ test_that("generate_manifest includes correct columns", {
   
 })
 
+test_that("generate_key/value_description includes correct columns", {
+  dat <- with_mock(
+    get_synapse_annotations = function() {
+      load(system.file("testdata", "annotations.RData", package = "syndccutils"))
+      return(annotations)
+    },
+    get_synapse_annotations()
+  )
+  key_desc_names <- c("key", "description", "columnType", "module")
+  val_desc_names <- c("key", "value", "valueDescription", "source", "module")
+  expect_equal(names(generate_key_description(dat)), key_desc_names)
+  expect_equal(names(generate_value_description(dat)), val_desc_names)
+})
+
+test_that("no duplicated rows in generate_key_description output", {
+  dat <- with_mock(
+    get_synapse_annotations = function() {
+      load(system.file("testdata", "annotations.RData", package = "syndccutils"))
+      return(annotations)
+    },
+    get_synapse_annotations()
+  )
+  desc <- generate_key_description(dat)
+  expect_false(any(duplicated(desc)))
+})
+
 test_that("write_manifest writes to excel file", {
+  x1 <- data.frame(x = 5)
+  x2 <- list(x1, x1)
+
+  tmp1 <- tempfile()
+  tmp2 <- tempfile()
+  on.exit(unlink(c(tmp1, tmp2)))
+  
+  write_manifest(x1, tmp1)
+  write_manifest(x2, tmp2)
+
+  expect_equal(openxlsx::read.xlsx(tmp1), x1)
+  expect_equal(
+    lapply(1:2, function(x) {openxlsx::read.xlsx(tmp2, sheet = x)}),
+    x2
+  )
+})
+
+test_that("write_manifest writes passes ... to write.xlsx", {
   tmp <- tempfile()
   on.exit(unlink(tmp))
 
-  x <- data.frame(x = 1:5)
-  write_manifest(x, tmp)
+  dat <- data.frame(x = 1:5, y = 6:10)
+  write_manifest(dat, tmp, colNames = FALSE)
+  dat2 <- openxlsx::read.xlsx(tmp)
 
-  expect_equal(openxlsx::read.xlsx(tmp), x)
+  expect_false(any(c("x", "y") %in% names(dat2)))
+  
 })
